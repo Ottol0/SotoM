1eec30e56d48fc5f711a2b742f82b693062f2cd8
Otto10 <ottomaldonado01@gmail.com>
2022-05-04 13:19:33 -0500

    cambio

--- Parcial 2/Gato/Gato.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    document.getElementById("contenedor").addEventListener("click", function(evento) {
-        console.log(evento.target.id);
-
-        document.getElementById(evento.target.id).classList.toggle("cajaAmarilla");
-    });
-}
