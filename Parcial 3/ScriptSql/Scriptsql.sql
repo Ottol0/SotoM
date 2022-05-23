efdca2f59d482e0814e2106a62852f01c6367c1d
Otto10 <ottomaldonado01@gmail.com>
2022-05-23 14:42:57 -0500

    Script

--- Parcial 3/ScriptSql/Scriptsql.sql
+++ Parcial 3/ScriptSql/Scriptsql.sql
@@ -4,6 +4,7 @@ CREATE DATAbase l19100799;
 use l19100799
 
 create table registro(
+    IdRegistro int IDENTITY(1,1),
     Nombre VARCHAR(100),
     Autor varchar(20),
     Edicion varchar(15),
@@ -16,5 +17,12 @@ create table registro(
 insert into registro( Nombre, Autor, Edicion, Editorial, Genero, Fecha, Descripcion)
 VALUES ('Primera persona del singular', 'Haruki Murakami', 'Original','TUSQUETS','Romance','2021-10-12','Cuentos cortos');
 
+insert into registro( Nombre, Autor, Edicion, Editorial, Genero, Fecha, Descripcion)
+VALUES ('Ante el dolor de los demas', 'Susan Sontag', 'Original','ePub','Drama','2012-05-17','Ya no podemos ser inocentes, somos testigos');
+
+insert into registro( Nombre, Autor, Edicion, Editorial, Genero, Fecha, Descripcion)
+VALUES ('Adios a Las Armas', 'Ernest Hemingway', 'Original','SouleSpagnuolo','Drama','1982-03-10','Aquel año, al final del verano,viviamos en una casa de un pueblo mas alla del rio');
+
 select *
 FROM registro
+
